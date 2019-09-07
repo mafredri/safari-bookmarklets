818532669c4aa128650dba819e4a7b45b7299b52
Mathias Fredriksson <mafredri@gmail.com>
2019-09-07 12:58:06 +0300

    src/godoc.js: Refactor to new format

--- src/godoc.js
+++ src/godoc.js
@@ -1,6 +1,13 @@
-javascript:(function(){
-        /* This tries to handle most cases on GitHub. */
-        const re = /\/((tree|blob|commits?|issues|pull|graphs|settings|network)\/[^/]+|(issues|pulls|projects|wiki|pulse|community|network|settings)$)/;
-        const loc = window.location;
-        window.location = 'https://godoc.org/'+loc.hostname+loc.pathname.replace(re, '');
-})();void(0);
+/**
+ * GoDoc
+ *
+ * Navigates to GoDoc from the current repository, e.g. on GitHub.
+ *
+ * Author: Mathias Fredriksson
+ * Version: 0.0.0
+ */
+// This regexp tries to handle most cases on GitHub.
+const re = /\/((tree|blob|commits?|issues|pull|graphs|settings|network)\/[^/]+|(issues|pulls|projects|wiki|pulse|community|network|settings)$)/;
+const loc = window.location;
+window.location =
+	'https://godoc.org/' + loc.hostname + loc.pathname.replace(re, '');

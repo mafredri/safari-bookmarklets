828c4ab3cc0e16f46e3391071d3247be7faa90be
Mathias Fredriksson <mafredri@gmail.com>
2019-09-07 12:58:44 +0300

    src/google-translate.js: Add toggle functionality

--- src/google-translate.js
+++ src/google-translate.js
@@ -1,4 +1,30 @@
-javascript:(function(){
-        const href = encodeURIComponent(window.location.href);
-        window.location = 'https://translate.google.com/translate?sl=auto&tl=en&js=y&prev=_t&hl=en&ie=UTF-8&edit-text=&u=' + href;
-})();void(0);
+/**
+ * Google Translate
+ *
+ * Translates the current page using automatic language detection. When
+ * clicked on a translated page, will return to the original.
+ *
+ * Author: Mathias Fredriksson
+ * Version: 0.0.0
+ */
+if (!window.location.origin.includes('://translate.google.com')) {
+	translate();
+} else {
+	untranslate();
+}
+
+function translate() {
+	const href = encodeURIComponent(window.location.href);
+	const url =
+		'https://translate.google.com/translate?sl=auto&tl=en&js=y&prev=_t&hl=en&ie=UTF-8&edit-text=&u=' +
+		href;
+	window.location = url;
+}
+function untranslate() {
+	let url = window.location.search
+		.split('&')
+		.filter(qp => qp.startsWith('u='))[0]
+		.slice(2);
+	url = decodeURIComponent(url);
+	window.location = url;
+}

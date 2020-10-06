7b11af4a400076b0cfd2f3476fa6a5b488203129
Mathias Fredriksson <mafredri@gmail.com>
2020-10-07 01:18:48 +0300

    Add ios-youtube-pip.js
    All credits for the original code go to Code Everywhere.
    
    https://codeeverywhere.ca/post.php?id=56#Enable-YouTube-picture-in-picture-on-iOS-14-Safari-with-this-Siri-Shortcut

---
+++ src/ios-youtube-pip.js
@@ -0,0 +1,22 @@
+/**
+ * YouTube PiP
+ *
+ * Enable YouTube PiP on iOS devices.
+ *
+ * All credits for the original code go to Code Everywhere, from:
+ * https://codeeverywhere.ca/post.php?id=56#Enable-YouTube-picture-in-picture-on-iOS-14-Safari-with-this-Siri-Shortcut
+ *
+ * Author: Mathias Fredriksson
+ * Version: 0.0.1
+ */
+let v = document.querySelector('video');
+
+v.addEventListener(
+	'webkitpresentationmodechanged',
+	(e) => e.stopPropagation(),
+	true,
+);
+
+setTimeout(() => v.webkitSetPresentationMode('picture-in-picture'), 3000);
+
+completion();
